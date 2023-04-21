c367a93b786f753e0d9b3bf5b6dc1b90c5457d25
helga <helga-nikol09@yandex.ru>
2023-04-21 14:34:47 +0300

    first part

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+    public static void main(String[] args){
+        task1 ();
+        task2 ();
+        task3 ();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+    }
+    public static void task2 () {
+        System.out.println("Задача");
+        var dog = 8.0;
+        dog = dog + 4;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper);
+    }
+    public static void task3 () {
+        var dog = 8.0;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
